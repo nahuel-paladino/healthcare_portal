6d2a5249936a61451dbfbba10845cf920cb7a83a
nahuel-paladino <paladino.m.nahuel@gmail.com>
2024-03-27 15:11:17 -0600

    add tbody to tables to avoid warning

--- src/components/patient-card.js
+++ src/components/patient-card.js
@@ -4,38 +4,40 @@ export default function PatientCard() {
     return (
         <InfoCard title="Patient Information">
             <table>
-                <tr>
-                    <th>Name</th>
-                    <td>Jane Doe</td>
-                </tr>
-                <tr>
-                    <th>Gender</th>
-                    <td>She/Her</td>
-                </tr>
-                <tr>
-                    <th>Sex</th>
-                    <td>Female</td>
-                </tr>
-                <tr>
-                    <th>PHN</th>
-                    <td>12345-6789</td>
-                </tr>
-                <tr>
-                    <th>Date of Birth</th>
-                    <td>01/01/19XX</td>
-                </tr>
-                <tr>
-                    <th>Address</th>
-                    <td>123 Street Ave NE, A1B 2C3</td>
-                </tr>
-                <tr>
-                    <th>Immigrant Status</th>
-                    <td>Non-Immigrant</td>
-                </tr>
-                <tr>
-                    <th>Place of Birth</th>
-                    <td>Calgary, AB</td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <th>Name</th>
+                        <td>Jane Doe</td>
+                    </tr>
+                    <tr>
+                        <th>Gender</th>
+                        <td>She/Her</td>
+                    </tr>
+                    <tr>
+                        <th>Sex</th>
+                        <td>Female</td>
+                    </tr>
+                    <tr>
+                        <th>PHN</th>
+                        <td>12345-6789</td>
+                    </tr>
+                    <tr>
+                        <th>Date of Birth</th>
+                        <td>01/01/19XX</td>
+                    </tr>
+                    <tr>
+                        <th>Address</th>
+                        <td>123 Street Ave NE, A1B 2C3</td>
+                    </tr>
+                    <tr>
+                        <th>Immigrant Status</th>
+                        <td>Non-Immigrant</td>
+                    </tr>
+                    <tr>
+                        <th>Place of Birth</th>
+                        <td>Calgary, AB</td>
+                    </tr>
+                </tbody>
             </table>
         </InfoCard>
     )
--- src/components/provider-card.js
+++ src/components/provider-card.js
@@ -4,30 +4,32 @@ export default function ProviderCard() {
     return (
         <InfoCard title="Provider Information">
             <table>
-                <tr>
-                    <th>Name</th>
-                    <td>Dr. Taylor Smith</td>
-                </tr>
-                <tr>
-                    <th>Clinic</th>
-                    <td>Neighbourhood Clinic</td>
-                </tr>
-                <tr>
-                    <th>Location</th>
-                    <td>Calgary, AB</td>
-                </tr>
-                <tr>
-                    <th>Phone</th>
-                    <td>(403)123-4567</td>
-                </tr>
-                <tr>
-                    <th>Fax</th>
-                    <td>(403)098-7654</td>
-                </tr>
-                <tr>
-                    <th>Email</th>
-                    <td>t.smith@clinic.com</td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <th>Name</th>
+                        <td>Dr. Taylor Smith</td>
+                    </tr>
+                    <tr>
+                        <th>Clinic</th>
+                        <td>Neighbourhood Clinic</td>
+                    </tr>
+                    <tr>
+                        <th>Location</th>
+                        <td>Calgary, AB</td>
+                    </tr>
+                    <tr>
+                        <th>Phone</th>
+                        <td>(403)123-4567</td>
+                    </tr>
+                    <tr>
+                        <th>Fax</th>
+                        <td>(403)098-7654</td>
+                    </tr>
+                    <tr>
+                        <th>Email</th>
+                        <td>t.smith@clinic.com</td>
+                    </tr>
+                </tbody>
             </table>
         </InfoCard>
     )
